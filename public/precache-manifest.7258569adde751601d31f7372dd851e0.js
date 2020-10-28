2b27dffa32296c127bc0b57d4d58223e40b844ea
Ashish Kumar Giri <ashishgiri448@gmail.com>
2020-10-29 00:25:40 +0530

    adding files

---
+++ public/precache-manifest.7258569adde751601d31f7372dd851e0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e6cc6b8695499d6ac0bc176475e91c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dafcbaf2eb3b8c27498",
+    "url": "/static/css/0.ef72e3fb.chunk.css"
+  },
+  {
+    "revision": "8e593d7e9f6ad97ab9bd",
+    "url": "/static/css/4.e287ac20.chunk.css"
+  },
+  {
+    "revision": "74e4a1e5c6d2970302af",
+    "url": "/static/css/5.2224dad0.chunk.css"
+  },
+  {
+    "revision": "b86982698279f30b22a4",
+    "url": "/static/css/6.51845c35.chunk.css"
+  },
+  {
+    "revision": "21046f4211de1532b7d4",
+    "url": "/static/css/7.25981807.chunk.css"
+  },
+  {
+    "revision": "a45ce7b16628708364f4",
+    "url": "/static/css/8.4c388a3f.chunk.css"
+  },
+  {
+    "revision": "b8cae2fce007b0df1ad5",
+    "url": "/static/css/main.08b7a5b4.chunk.css"
+  },
+  {
+    "revision": "6dafcbaf2eb3b8c27498",
+    "url": "/static/js/0.e7ebabfd.chunk.js"
+  },
+  {
+    "revision": "692d6a164e49b85d643c",
+    "url": "/static/js/3.e1d6759e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.e1d6759e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e593d7e9f6ad97ab9bd",
+    "url": "/static/js/4.95b3c137.chunk.js"
+  },
+  {
+    "revision": "74e4a1e5c6d2970302af",
+    "url": "/static/js/5.249ca9f2.chunk.js"
+  },
+  {
+    "revision": "b86982698279f30b22a4",
+    "url": "/static/js/6.b348a65c.chunk.js"
+  },
+  {
+    "revision": "21046f4211de1532b7d4",
+    "url": "/static/js/7.6eaa803a.chunk.js"
+  },
+  {
+    "revision": "a45ce7b16628708364f4",
+    "url": "/static/js/8.a2c03b33.chunk.js"
+  },
+  {
+    "revision": "b8cae2fce007b0df1ad5",
+    "url": "/static/js/main.663842f1.chunk.js"
+  },
+  {
+    "revision": "6d52f2136bc87f0d9659",
+    "url": "/static/js/runtime-main.45194654.js"
+  }
+]);
